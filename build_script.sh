1d6b9075fd8d03f2a2609b7d2e8c90adbc9f0f4d
kannanb95 <142100996+kannanb95@users.noreply.github.com>
2023-09-26 23:53:01 +0530

    modified

--- build_script.sh
+++ build_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#git clone
+#git clone 
 git clone https://github.com/kannanb95/testprojectrepo.git
 
-#to cloned repo
+#to the cloned repo dir
 cd testprojectrepo
 
 #build docker
@@ -13,7 +13,10 @@ docker build -t testproject:latest .
 docker login
 #push docker
 docker push kannanb95/testproject:latest
-
+#stop the container
+docker stop projectrepo_container
+#remove the container
+docker rm projectrepo_container
 #docker container
 docker run -d --name projectrepo_container -p 8080:80 testproject:latest
 
