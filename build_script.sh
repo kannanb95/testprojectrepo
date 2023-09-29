65ad1cd172bdd1b53a372ec848a93b646e9b6c67
kannanb95 <142100996+kannanb95@users.noreply.github.com>
2023-09-29 06:54:51 +0530

    modified buildscript

--- build_script.sh
+++ build_script.sh
@@ -41,7 +41,7 @@ docker stop projectrepo_container
 #remove the container
 docker container rm projectrepo_container
 #docker container
-docker run -d --name projectrepo_container -p 8080:80 testproject
+docker run -d --name projectrepo_container -p 8080:80 testproject:"$TAG" "$VERSION"
 else
 	echo "No changes on $REMOTE/$BRANCH"
 fi 
